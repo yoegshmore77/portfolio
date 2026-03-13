6050623f0ba15371e62fc83ceffca9b28aea49f8
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-13 18:25:39 +0530

    Add files via upload

--- myprojects/lenovo/detector_31.js
+++ myprojects/lenovo/detector_31.js
@@ -982,7 +982,7 @@ const ok =
     normAspect > 1.25 &&
     //aspect > 1.15 //&&
     //aspect > 1 ;//&&
-    normAspect < 20.0 && w > 50 && w < 150;
+    normAspect < 20.0 && w > 50 && w < 350;
 
                 const tag = `v${v} f${fill.toFixed(2)} a${aspect.toFixed(1)}`;
                 stats.info.push(tag + (ok ? ' ✓' : ''));
