fd6c79fcf5b3a8fe0016c20c17736ef81e8731c0
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-15 11:51:54 +0530

    Add files via upload

--- myprojects/lenovo/detector_41_test.js
+++ myprojects/lenovo/detector_41_test.js
@@ -577,8 +577,8 @@ if (br.width <= br.height) continue; // reject portrait or square
 
                 
                 scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
-                scan_status_msg.style.color = "yellow";
-                //scan_status_msg.style.color = "white";
+                //scan_status_msg.style.color = "yellow";
+                scan_status_msg.style.color = "white";
 
                 // TIGHTER CHECK:
                 //   4-8 vertices, fill > 0.3, solidity > 0.85, landscape aspect 1.2-4.0
@@ -622,7 +622,7 @@ const centerToleranceY = roiH * 0.20;
 
 const ok =
     v >= 4 && v <= 8 &&
-    fill > 0.45 &&
+    fill > 0.65 &&
     //solidity > 0.65 && // solidity > 0.65 &&
     normAspect > 1.25 &&
     normAspect < 4.0 &&
