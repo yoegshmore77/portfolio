b4b894ea8a413587317d74f24c61a18a7018a9fd
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-15 11:02:21 +0530

    Add files via upload

--- myprojects/lenovo/detector_32.js
+++ myprojects/lenovo/detector_32.js
@@ -991,8 +991,8 @@ const aspect = br.width / br.height;
                 //scan_status_msg.innerHTML = Math.round(aspect) + " = " + Math.round(w) + " = " + Math.round(solidity) + " = " + Math.round(fill);
                 scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
                 //scan_status_msg.style.color = "yellow";
-                scan_status_msg.style.color = "white";
-                //scan_status_msg.style.color = "orange";
+                //scan_status_msg.style.color = "white";
+                scan_status_msg.style.color = "orange";
 
                 // TIGHTER CHECK:
                 //   4-8 vertices, fill > 0.3, solidity > 0.85, landscape aspect 1.2-4.0
@@ -1040,7 +1040,7 @@ const ok =
     v >= 4 && v <= 8 &&
     //fill > 0.45 &&
     //fill > 0.35 &&
-    fill > 0.75 &&
+    fill > 0.45 &&
     //solidity > 0.65 &&
     normAspect > 1.25 &&
     normAspect < 4.0 &&
