da63c39f8ef7d999d19d4fe59bd9a4ea8d785879
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-15 07:18:20 +0530

    Add files via upload

--- myprojects/lenovo/detector_32.js
+++ myprojects/lenovo/detector_32.js
@@ -974,6 +974,7 @@ if (br.width <= br.height) continue; // reject portrait or square
 
 
                 //const normAspect = aspect > 1 ? aspect : 1 / aspect;
+                const normAspect = aspect;
 
 
 
@@ -982,8 +983,8 @@ if (br.width <= br.height) continue; // reject portrait or square
                 //scan_status_msg.innerHTML = Math.round(aspect) + " = " + Math.round(w) + " = " + Math.round(solidity) + " = " + Math.round(fill);
                 scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
                 //scan_status_msg.style.color = "yellow";
-                scan_status_msg.style.color = "white";
-                //scan_status_msg.style.color = "orange";
+                //scan_status_msg.style.color = "white";
+                scan_status_msg.style.color = "orange";
 
                 // TIGHTER CHECK:
                 //   4-8 vertices, fill > 0.3, solidity > 0.85, landscape aspect 1.2-4.0
