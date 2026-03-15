c9c1cdf9b42d873c673270499bded859e0b0a112
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-15 10:01:30 +0530

    Add files via upload

--- myprojects/lenovo/detector_32.js
+++ myprojects/lenovo/detector_32.js
@@ -987,9 +987,9 @@ const aspect = br.width / br.height;
 
                 //scan_status_msg.innerHTML = Math.round(aspect) + " = " + Math.round(w) + " = " + Math.round(solidity) + " = " + Math.round(fill);
                 scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
-                scan_status_msg.style.color = "yellow";
+                //scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
-                //scan_status_msg.style.color = "orange";
+                scan_status_msg.style.color = "orange";
 
                 // TIGHTER CHECK:
                 //   4-8 vertices, fill > 0.3, solidity > 0.85, landscape aspect 1.2-4.0
@@ -1060,6 +1060,10 @@ const ok =
 
 
                 if (ok) {
+
+                scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
+
+
                     stats.ok++;
                     if (area > bestArea) {
                         bestArea = area;
@@ -1186,7 +1190,7 @@ let dummy_video_grab = null;
         if (lastRect) {
             const d = Math.hypot(rect.cx - lastRect.cx, rect.cy - lastRect.cy);
             //stabilityCounter = d < 50 ? stabilityCounter + 1 : Math.max(0, stabilityCounter - 1);
-            stabilityCounter = d < 50 ? stabilityCounter + 1 : Math.max(0, stabilityCounter - 1);
+            stabilityCounter = d < 80 ? stabilityCounter + 1 : Math.max(0, stabilityCounter - 1);
         } else {
             stabilityCounter = 1;
         }
