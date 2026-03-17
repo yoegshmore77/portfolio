ae900eac811719e891eafce168de5ffa76dea078
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-17 17:18:31 +0530

    Add files via upload

--- myprojects/lenovo/detector_45.js
+++ myprojects/lenovo/detector_45.js
@@ -588,8 +588,8 @@ if (br.width <= br.height) continue; // reject portrait or square
                 scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
                 //scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
-                scan_status_msg.style.color = "orange";
-                //scan_status_msg.style.color = "red";
+                //scan_status_msg.style.color = "orange";
+                scan_status_msg.style.color = "red";
 
                 // TIGHTER CHECK:
                 //   4-8 vertices, fill > 0.3, solidity > 0.85, landscape aspect 1.2-4.0
@@ -643,10 +643,10 @@ const ok =
     normAspect < 14.0 &&
     //w > 60 &&
     //w < roiW * 0.9 &&
-    //longSide > 60 &&
-    longSide > 45 &&
+    longSide > 60 &&
+    //longSide > 45 &&
     //longSide < roiW * 0.6 &&
-    longSide < roiW * 0.9 &&
+    longSide < roiW * 0.6 &&
     //shortSide > 30 &&
     dx < centerToleranceX &&
     dy < centerToleranceY;
@@ -785,7 +785,7 @@ let dummy_video_grab = null;
         if (lastRect) {
             const d = Math.hypot(rect.cx - lastRect.cx, rect.cy - lastRect.cy);
             //stabilityCounter = d < 50 ? stabilityCounter + 1 : Math.max(0, stabilityCounter - 1);
-            stabilityCounter = d < 25 ? stabilityCounter + 1 : Math.max(0, stabilityCounter - 1);
+            stabilityCounter = d < 50 ? stabilityCounter + 10 : Math.max(0, stabilityCounter - 1);
         } else {
             stabilityCounter = 1;
         }
