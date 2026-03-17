ffe8c5265bf8a9a2abcfebc4e450d5ea64e4c28b
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-17 15:49:44 +0530

    Add files via upload

--- myprojects/lenovo/detector_45.js
+++ myprojects/lenovo/detector_45.js
@@ -518,7 +518,7 @@ document.body.style.fontFamily = 'sans-serif';
 
                 //if (area < minA || area > maxA) continue;
 
-                if (area < roiW * roiH * 0.005) continue;
+                //if (area < roiW * roiH * 0.005) continue;
 
                 const br = cv.boundingRect(cnt);
 
@@ -587,8 +587,8 @@ if (br.width <= br.height) continue; // reject portrait or square
                 
                 scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
                 //scan_status_msg.style.color = "yellow";
-                scan_status_msg.style.color = "white";
-                //scan_status_msg.style.color = "orange";
+                //scan_status_msg.style.color = "white";
+                scan_status_msg.style.color = "orange";
                 //scan_status_msg.style.color = "red";
 
                 // TIGHTER CHECK:
