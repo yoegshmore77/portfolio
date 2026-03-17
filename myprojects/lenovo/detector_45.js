c10fcdd19eb5c6cacf6c1b5f331ad43822534647
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-17 17:59:03 +0530

    Add files via upload

--- myprojects/lenovo/detector_45.js
+++ myprojects/lenovo/detector_45.js
@@ -586,8 +586,8 @@ if (br.width <= br.height) continue; // reject portrait or square
 
                 
                 scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
-                //scan_status_msg.style.color = "yellow";
-                scan_status_msg.style.color = "white";
+                scan_status_msg.style.color = "yellow";
+                //scan_status_msg.style.color = "white";
                 //scan_status_msg.style.color = "orange";
                 //scan_status_msg.style.color = "red";
 
@@ -781,11 +781,13 @@ let dummy_video_grab = null;
     // ── Stability → Lock ─────────────────────────────────────────
     let base64Image = null;
 
+    LOCK_TARGET = 5;
+
     function checkStability(rect) {
         if (lastRect) {
             const d = Math.hypot(rect.cx - lastRect.cx, rect.cy - lastRect.cy);
             //stabilityCounter = d < 50 ? stabilityCounter + 1 : Math.max(0, stabilityCounter - 1);
-            stabilityCounter = d < 50 ? stabilityCounter + 5 : Math.max(0, stabilityCounter - 5);
+            stabilityCounter = d < 50 ? stabilityCounter + 1 : Math.max(0, stabilityCounter - 1);
         } else {
             stabilityCounter = 1;
         }
