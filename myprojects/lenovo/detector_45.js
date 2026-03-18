10cc9a0794f14e3ac66b7bb549c7c0107dd22f08
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-18 18:55:50 +0530

    Add files via upload

--- myprojects/lenovo/detector_45.js
+++ myprojects/lenovo/detector_45.js
@@ -595,8 +595,8 @@ if (br.width <= br.height) continue; // reject portrait or square
                 //scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
                 //scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
-                //scan_status_msg.style.color = "orange";
-                scan_status_msg.style.color = "red";
+                scan_status_msg.style.color = "orange";
+                //scan_status_msg.style.color = "red";
 
                 // TIGHTER CHECK:
                 //   4-8 vertices, fill > 0.3, solidity > 0.85, landscape aspect 1.2-4.0
@@ -822,7 +822,7 @@ let dummy_video_grab = null;
             //stabilityCounter = d < 50 ? stabilityCounter + 1 : Math.max(0, stabilityCounter - 1);
             stabilityCounter = d < 50 ? stabilityCounter + 1 : Math.max(0, stabilityCounter - 1);
         } else {
-            stabilityCounter = 1;
+            //stabilityCounter = 1;
         }
         lastRect = rect;
 
