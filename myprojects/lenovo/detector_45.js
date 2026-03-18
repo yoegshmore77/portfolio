7e075cfd869a8bfce3aaafdb781e8a7bfc9d7709
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-18 16:22:14 +0530

    Add files via upload

--- myprojects/lenovo/detector_45.js
+++ myprojects/lenovo/detector_45.js
@@ -677,8 +677,8 @@ const ok =
                 //scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
                 //scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
-                scan_status_msg.style.color = "orange";
-                //scan_status_msg.style.color = "red";
+                //scan_status_msg.style.color = "orange";
+                scan_status_msg.style.color = "red";
 
                     stats.ok++;
                     if (area > bestArea) {
@@ -829,7 +829,8 @@ let dummy_video_grab = null;
             const pct = Math.floor((stabilityCounter / LOCK_TARGET) * 100);
             statusMsg.textContent = `LOCKING... ${pct}%`;
             statusMsg.style.color = 'yellow';
-        } else {
+        } //else 
+        if (stabilityCounter >= LOCK_TARGET) {
             // ═══ LOCKED ═══ ==== ==== ==== ==== ==== ==== ==== ==== ==== ====
 
             dbgCanvas.style.opacity = 1; 
