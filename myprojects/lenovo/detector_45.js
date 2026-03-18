607fa0f3004e7a8d972f86863e182b66438b44bd
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-18 18:31:34 +0530

    Add files via upload

--- myprojects/lenovo/detector_45.js
+++ myprojects/lenovo/detector_45.js
@@ -678,7 +678,7 @@ const ok =
                 //scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
                 //scan_status_msg.style.color = "orange";
-                scan_status_msg.style.color = "red";
+                //scan_status_msg.style.color = "red";
 
                     stats.ok++;
                     if (area > bestArea) {
