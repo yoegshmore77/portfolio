05b5ca2f1b31d9f847d1f4f32752978034d6c3bb
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-18 18:51:59 +0530

    Add files via upload

--- myprojects/lenovo/detector_45.js
+++ myprojects/lenovo/detector_45.js
@@ -596,7 +596,7 @@ if (br.width <= br.height) continue; // reject portrait or square
                 //scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
                 //scan_status_msg.style.color = "orange";
-                //scan_status_msg.style.color = "red";
+                scan_status_msg.style.color = "red";
 
                 // TIGHTER CHECK:
                 //   4-8 vertices, fill > 0.3, solidity > 0.85, landscape aspect 1.2-4.0
@@ -680,6 +680,7 @@ const ok =
                 //scan_status_msg.style.color = "orange";
                 //scan_status_msg.style.color = "red";
 
+                    stabilityCounter++;
                     stats.ok++;
                     if (area > bestArea) {
                         bestArea = area;
