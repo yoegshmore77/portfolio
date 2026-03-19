c99686d4721aa4e08bfcb7b0d087161921cbcebc
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-19 20:13:45 +0530

    Add files via upload

--- myprojects/lenovo/detector_52.js
+++ myprojects/lenovo/detector_52.js
@@ -523,7 +523,7 @@ document.body.style.fontFamily = 'sans-serif';
                 const cnt = contours.get(i);
                 
         const area = cv.contourArea(cnt);
-        if (area < roiW * roiH * 0.02) {
+        if (area < roiW * roiH * 0.01) {
             cnt.delete();
             continue;
         }
@@ -682,8 +682,8 @@ const ok =
                 //scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
                 //scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
-                //scan_status_msg.style.color = "orange";
-                scan_status_msg.style.color = "red";
+                scan_status_msg.style.color = "orange";
+                //scan_status_msg.style.color = "red";
 
                     //stabilityCounter++;
                     stats.ok++;
