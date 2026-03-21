eb20630a5446f6636d62ae91fcae573cca4a0dd7
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-21 12:31:17 +0530

    Add files via upload

--- myprojects/lenovo/detector_52.js
+++ myprojects/lenovo/detector_52.js
@@ -664,7 +664,7 @@ const ok =
     longSide < roiW * 0.6 &&
     //longSide < roiW * 0.6 &&
     //shortSide > 30 &&
-    rectangularity > 0.2 &&
+    rectangularity > 0.22 &&
     dx < centerToleranceX &&
     dy < centerToleranceY;
 
@@ -685,11 +685,11 @@ const ok =
                 if (ok) {
 
                 //scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
-                //scan_status_msg.style.color = "yellow";
+                scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
                 //scan_status_msg.style.color = "orange";
                 //scan_status_msg.style.color = "red";
-                scan_status_msg.style.color = "blue";
+                //scan_status_msg.style.color = "blue";
 
                     //stabilityCounter++;
                     stats.ok++;
