a8f83186ea7688ce85d9823bf41bf84fa24c0c45
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-21 18:35:53 +0530

    Add files via upload

--- myprojects/lenovo/detector_51.js
+++ myprojects/lenovo/detector_51.js
@@ -695,7 +695,7 @@ function runCV(vw, vh, roiX, roiY, roiW, roiH) {
         const v = approx.rows;
 
         // 🔥 STRICT: must be 4 corners
-        if (v >= 4) {
+        if (v !== 4) {
             approx.delete();
             cnt.delete();
             continue;
@@ -780,7 +780,7 @@ function runCV(vw, vh, roiX, roiY, roiW, roiH) {
 
             //checkStability(rect);
 
-                scan_status_msg.style.color = "green";
+                scan_status_msg.style.color = "yellow";
 
                     //stats.ok++;
                     if (area > bestArea) {
