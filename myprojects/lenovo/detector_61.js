dbbf74499fbd34285370635a51b6bb70e22c78b8
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-22 11:59:34 +0530

    Add files via upload

--- myprojects/lenovo/detector_61.js
+++ myprojects/lenovo/detector_61.js
@@ -597,7 +597,7 @@ if (br.width <= br.height) continue; // reject portrait or square
 
                 
                 //scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
-                scan_status_msg.style.color = "yellow";
+                //scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
                 //scan_status_msg.style.color = "orange";
                 //scan_status_msg.style.color = "red";
@@ -682,7 +682,7 @@ const ok =
                 //scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
                 //scan_status_msg.style.color = "orange";
-                scan_status_msg.style.color = "red";
+                //scan_status_msg.style.color = "red";
 
                     //stabilityCounter++;
                     stats.ok++;
