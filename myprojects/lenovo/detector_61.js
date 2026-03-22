c450ff41cdf53c046eae542c206bbbca0990c006
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-22 10:52:15 +0530

    Add files via upload

--- myprojects/lenovo/detector_61.js
+++ myprojects/lenovo/detector_61.js
@@ -523,7 +523,7 @@ document.body.style.fontFamily = 'sans-serif';
                 const cnt = contours.get(i);
                 
                 const area = cv.contourArea(cnt);
-                //if (area < 5000) continue;  // tune this
+                if (area < 500) continue;  // tune this
 
                 //if (area < minA || area > maxA) continue;
 
@@ -571,7 +571,7 @@ if (br.width <= br.height) continue; // reject portrait or square
                 const approx = new cv.Mat();
                 //cv.approxPolyDP(cnt, approx, 0.04 * peri, true);
                 //if (!cv.isContourConvex(cnt)) continue;
-                if (!cv.isContourConvex(approx)) continue;
+                if (!cv.isContourConvex(cnt)) continue;
                 
                 //cv.approxPolyDP(cnt, approx, 0.02 * peri, true);
                 cv.approxPolyDP(cnt, approx, 0.01 * peri, true);
@@ -599,8 +599,8 @@ if (br.width <= br.height) continue; // reject portrait or square
                 //scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
                 //scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
-                //scan_status_msg.style.color = "orange";
-                scan_status_msg.style.color = "red";
+                scan_status_msg.style.color = "orange";
+                //scan_status_msg.style.color = "red";
 
                 // TIGHTER CHECK:
                 //   4-8 vertices, fill > 0.3, solidity > 0.85, landscape aspect 1.2-4.0
