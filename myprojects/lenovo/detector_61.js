9d74085c8377b675c1180be723aff5e535aad10b
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-22 10:59:45 +0530

    Add files via upload

--- myprojects/lenovo/detector_61.js
+++ myprojects/lenovo/detector_61.js
@@ -523,7 +523,7 @@ document.body.style.fontFamily = 'sans-serif';
                 const cnt = contours.get(i);
                 
                 const area = cv.contourArea(cnt);
-                if (area < 50) continue;  // tune this
+                //if (area < 50) continue;  // tune this
 
                 //if (area < minA || area > maxA) continue;
 
@@ -599,8 +599,8 @@ if (br.width <= br.height) continue; // reject portrait or square
                 //scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
                 //scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
-                scan_status_msg.style.color = "orange";
-                //scan_status_msg.style.color = "red";
+                //scan_status_msg.style.color = "orange";
+                scan_status_msg.style.color = "red";
 
                 // TIGHTER CHECK:
                 //   4-8 vertices, fill > 0.3, solidity > 0.85, landscape aspect 1.2-4.0
