575ec195d4a25eec9e782abcd8c6a235982f0a67
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2026-03-22 11:22:58 +0530

    Add files via upload

--- myprojects/lenovo/detector_61.js
+++ myprojects/lenovo/detector_61.js
@@ -574,7 +574,7 @@ if (br.width <= br.height) continue; // reject portrait or square
                 //if (!cv.isContourConvex(cnt)) continue;
                 
                 //cv.approxPolyDP(cnt, approx, 0.02 * peri, true);
-                cv.approxPolyDP(cnt, approx, 0.015 * peri, true);
+                cv.approxPolyDP(hh[3], approx, 0.01 * peri, true);
                 const v = approx.rows;
                 approx.delete();
 
@@ -597,10 +597,10 @@ if (br.width <= br.height) continue; // reject portrait or square
 
                 
                 //scan_status_msg.innerHTML = aspect.toFixed(2) + " = " + w.toFixed(2) + " = " + solidity.toFixed(2) + " = " + fill.toFixed(2);
-                //scan_status_msg.style.color = "yellow";
+                scan_status_msg.style.color = "yellow";
                 //scan_status_msg.style.color = "white";
                 //scan_status_msg.style.color = "orange";
-                scan_status_msg.style.color = "red";
+                //scan_status_msg.style.color = "red";
 
                 // TIGHTER CHECK:
                 //   4-8 vertices, fill > 0.3, solidity > 0.85, landscape aspect 1.2-4.0
